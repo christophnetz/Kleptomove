9ffca75b892512152a2a280f42b83c112f4522a8
pratikunterwegs <pratikgupte16@gmail.com>
2020-10-29 18:19:45 +0100

    handle random agents case correctly

--- Rnotebooks/make_batfile.R
+++ Rnotebooks/make_batfile.R
@@ -3,18 +3,27 @@
 library(glue)
 library(stringr)
 library(tidyr)
+library(dplyr)
 
 runs <- str_pad(seq_len(3), width = 3, pad = "0")
-strategy <- c("foragers", "obligate", 
-                "facultative", "random")
+strategy <- c(
+  # "foragers", 
+  # "obligate", 
+  # "facultative", 
+  "random"
+)
 
 regrowth <- c(0.001, 0.01, 0.03, 0.05, 0.1)
+
+mask <- "{0,0,0}"
 
 # make data
 data_param <- crossing(replicate = runs, strategy, regrowth) %>% 
   mutate(outdir = glue('{strategy}_{replicate}'),
          agents.forage = as.numeric(strategy == "foragers"),
          agents.obligate = as.numeric(strategy == "obligate"))
+
+
 
 # prepare lines
 lines <- glue_data(data_param, 'kleptomove config=../settings/config.ini \\
@@ -24,7 +33,11 @@ lines <- glue_data(data_param, 'kleptomove config=../settings/config.ini \\
                    agents.handling_time=5 outdir=\\
                    ../../data/sim_{strategy}_rep_{replicate}_gro_{regrowth}')
 
+lines[data_param$strategy == "random"] <- 
+  glue('{lines[data_param$strategy == "random"]} \\
+       agents.input_mask={{0,0,0}')
+
 # write to file
 library(readr)
 write_lines(lines,
-            path = "bin/Release/runs_26_oct_2020.bat")
+            path = "bin/Release/runs_29_oct_2020_random.bat")
